b17b3932127d6a2d7f39e055a3e81ff83808911f
Lounès Ksouri <dev@louneskmt.com>
2020-12-29 23:56:01 +0400

    Use HOST env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 cd /lnbits
 ./venv/bin/quart assets
 ./venv/bin/quart migrate
-./venv/bin/hypercorn -k trio --bind 0.0.0.0:${PORT} 'lnbits.app:create_app()'
+./venv/bin/hypercorn -k trio --bind ${HOST}:${PORT} 'lnbits.app:create_app()'
